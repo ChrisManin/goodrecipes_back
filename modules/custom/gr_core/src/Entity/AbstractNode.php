660f26a1fc7709e877e828535f3b89081f4ee63c
Christophe Manin <christophe.manin@rcf.fr>
2025-10-11 18:35:37 +0200

    première class bundle de node

---
+++ modules/custom/gr_core/src/Entity/AbstractNode.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\gr_core\Entity;
+
+use Drupal\Core\Url;
+use Drupal\node\Entity\Node;
+
+abstract class AbstractNode extends Node
+{
+  public function getVisualFile(string $field_name): ?File {
+    return $this->$field_name
+    ?->first()
+    ?->get('entity')
+    ?->getTarget()
+    ?->getValue();
+  }
+
+  public function getRestVisual(string $field_name): array
+  {
+    $rest = [
+      "url" => '',
+      "alt" => '',
+    ];
+
+    $visual = $this->getVisualFile($field_name);
+
+    if ($visual) {
+      $rest["url"] = $visual->buildImageStyleUri();
+      $rest["alt"] = $this->get($field_name)->first()->get('alt')->getValue();
+    }
+
+    return $rest;
+  }
+
+  /**
+   * Return node alias
+   * @return string
+   */
+  public function getAlias(): string {
+    if (!empty($this->get('path')->getValue()[0]['alias'])) {
+      return $this->get('path')->getValue()[0]['alias'];
+    }
+    return '';
+  }
+
+  public static function getNodeByAlias($alias) {
+    $path = \Drupal::service('path_alias.manager')->getPathByAlias('/' . $alias);
+    if ($path == $alias) {
+      return NULL;
+    }
+    $params = Url::fromUri("internal:" . $path)->getRouteParameters();
+    $entity_type = key($params);
+    return \Drupal::entityTypeManager()->getStorage($entity_type)->load($params[$entity_type]);
+  }
+}
